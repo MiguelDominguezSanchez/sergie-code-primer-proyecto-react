e25a41aaef2500df3678c76f63143d3cb56951ef
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-10 09:53:02 +0200

    useEffect y Fetch

---
+++ src/UsersApp.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { UserList } from './components/userList'
+
+export const UsersApp = () => {
+	const [endPoint, setendPoint] = useState('users')
+
+	const handleFetch = () => {
+		setendPoint('comments')
+	}
+
+	return (
+		<>
+			<h1>CPS Cmrs List: </h1>
+			<UserList endPoint={endPoint}></UserList>
+			<button onClick={handleFetch}>Here the API Cmrs cases are called</button>
+		</>
+	)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { PrimerComponente } from './PrimerComponente'
-// import { ContadorApp } from './ContadorApp'
 import './styles.css'
-import { ListadoApp } from './ListadoApp.'
+import { UsersApp } from './UsersApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <PrimerComponente
-			titulo='Esta sección es de Props de Studio'
-			subtitulo={123}
-		/> */}
-		{/* <ContadorApp value={0} /> */}
-
-		<ListadoApp />
+		<UsersApp />
 	</React.StrictMode>
 )
